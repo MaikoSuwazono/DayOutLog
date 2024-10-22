3691ad94854436bfd183400d204cfad9a69a897f
maikogit config --global user.name maikogit config --global user.name maiko <suwamai00@gmail.com>
2024-10-22 23:58:56 +0900

    記事投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, success: "記事が投稿されました"
+    else
+      flash.now[:danger] = "記事の作成に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :departure_date)
   end
 end
