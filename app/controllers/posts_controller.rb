f6b2b3c063bbe243add1ad7a0b2534188c29062f
maikogit config --global user.name maikogit config --global user.name maiko <suwamai00@gmail.com>
2024-11-05 19:06:48 +0900

    新しい行き先のスタイル、バリデーションエラーが表示されるよう修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :departure_date, post_details_attributes: [:id, :body, :arrival_at, :image, :address, :latitude, :longitude])
+    params.require(:post).permit(:title, :departure_date, :image, post_details_attributes: [:id, :body, :arrival_at, :image, :address, :latitude, :longitude])
   end
 end
--- app/models/post_detail.rb
+++ app/models/post_detail.rb
@@ -1,5 +1,8 @@
 class PostDetail < ApplicationRecord
   belongs_to :post
 
+  validates :body, presence: true
+  validates :arrival_at, presence: true
+
   mount_uploader :image, PostDetailImageUploader
 end
